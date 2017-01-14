37ff7e1fe3e7e1c5da1b0c6e509735c6e250061f
Fernndo Viton <fcode@viton.org>
2017-01-13 23:31:42 -0800

    Add sample model/update/view

--- main.elm
+++ main.elm
@@ -1,7 +1,38 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, h1, div)
+import Html exposing ( Html, button, text, h1, div )
+import Html.Events exposing (onClick)
+import List exposing ( .. )
 
-main : Html a
-main = 
-  text "Hello World"
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+type alias Model = { fibNumbers : List Int }
+
+model : Model
+model =
+  { fibNumbers = [1,1]}
+
+
+-- UPDATE
+type Msg = AddFib
+
+update : Msg -> Model -> Model
+update msg model = 
+  case msg of
+    AddFib -> 
+      let 
+        reversed = reverse model.fibNumbers
+        sumEnd = sum (take 2 reversed)
+        newFibNumbers = append model.fibNumbers [sumEnd]
+      in { model | fibNumbers = newFibNumbers }
+
+
+-- VIEW
+view: Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick AddFib ] [ text "+" ]
+    , text (toString model.fibNumbers)
+    ]
